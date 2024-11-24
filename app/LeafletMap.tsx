151b17a9b73379fc1d464be916acb8ca8701ba6e
gunsow <fukudatatsuya911@gmail.com>
2024-11-24 15:50:47 +0900

    不要なファイルを削除した

--- app/LeafletMap.tsx
+++ app/LeafletMap.tsx
@@ -12,16 +12,12 @@ import { FormProvider, useForm } from "react-hook-form";
 import LegendControl from "./LegendControl";
 import { useSiteData } from "./useSiteData";
 import L from "leaflet";
-import { LocationMarker } from "./LocationMarker";
-import LocationLayerControl from "./LocationLayerControl";
-import { useState } from "react";
 L.Icon.Default.imagePath =
   "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/";
 
 export default function LeafletMap() {
   const iseki = useIsekiData();
   const site = useSiteData();
-  const [position, setPosition] = useState<{ lat: number; lng: number }>();
 
   const form = useForm<EraInput>({
     reValidateMode: "onSubmit",
@@ -90,18 +86,6 @@ export default function LeafletMap() {
         )}
         <EraControl />
         <LegendControl />
-        {/*
-        <LocationMarker
-          position={position}
-          onLocationChange={(lat, lng) => setPosition({ lat, lng })}
-        />
-        */}
-        {/* iseki && site && (
-          <LocationLayerControl
-            geoJsonList={[iseki, site]}
-            position={position}
-          />
-        )*/}
       </MapContainer>
     </FormProvider>
   );
